d73439d08ce71298b1a52ce98082b68069166bf4
rayhook <koohyar@gmail.com>
2023-05-16 12:23:09 -0300

    Create About route

--- src/components/About.tsx
+++
@@ -1,5 +0,0 @@
-function About() {
-  return <div>Hello from About!</div>;
-}
-
-export default About;
---
+++ src/routes/About.tsx
@@ -0,0 +1,11 @@
+function About() {
+  return (
+    <div className="h-screen">
+      <div className="text-3xl text-white h-full flex justify-center items-center">
+        Hello from About!
+      </div>
+    </div>
+  );
+}
+
+export default About;
