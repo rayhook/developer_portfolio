5afc7ea048e52aaffcfe7e9eb2ec636eb54d6c73
rayhook <koohyar@gmail.com>
2023-06-06 11:44:47 -0300

    Add filter by structure

--- src/routes/Filter.tsx
+++ src/routes/Filter.tsx
@@ -3,18 +3,24 @@ import nobelprizes from "../data/nobel.json";
 
 function Filter() {
   const [nobel, setNobel] = useState(nobelprizes);
-  console.log("nobel", nobel.nobelPrizes[0].laureates[0]);
+  const [sortby, setSortby] = useState("awardYear");
 
-  const sorted = [...nobel.nobelPrizes].sort(
-    (a, b) => a.laureates[0].fullName.en - b.laureates[0].fullName.en
-  );
+  const selectSortType = (sortBy: string) => {
+    setSortby(sortBy);
+  };
+
+  const sorted = [...nobel.nobelPrizes].sort((a, b) => b[sortby] - a[sortby]);
+
   return (
     <div className="flex flex-col w-full">
       <div className="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
         <div className="">
           <div className="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8 bg-yellow-500"></div>
           <div className="px-4 py-2 bg-green-400">
-            <button className="py-4 px-2 mx-2 rounded-md bg-slate-400">
+            <button
+              className="py-4 px-2 mx-2 rounded-md bg-slate-400"
+              onClick={() => selectSortType("name")}
+            >
               name
             </button>
             <button className="py-4 px-2 mx-2 rounded-md bg-slate-400">
