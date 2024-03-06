0d6410632671f286368509376bef409d1a11f886
rayhook <koohyar@gmail.com>
2024-03-05 20:00:38 -0400

    Remove unused routes

--- src/App.tsx
+++ src/App.tsx
@@ -6,22 +6,8 @@ function App() {
   const queryClient = new QueryClient();
   return (
     <QueryClientProvider client={queryClient}>
-      <div className="bg-orange-500">
-        <div className="px-60 py-8 bg-slate-800 sticky top-0 left-0 flex justify-between align-middle bg-opacity-50 text-white backdrop-blur-md">
-          <Link to="/">
-            <h1 className="text-2xl">Chris Movassagh</h1>
-          </Link>
-          <Link to="/about">
-            <h1 className="text-2xl">About</h1>
-          </Link>
-          <Link to="/filter">
-            <h1 className="text-2xl">Filter</h1>
-          </Link>
-        </div>
+      <div className="bg-indigo-100">
         <Outlet />
-        <div className="flex justify-center py-8 mt-4 bg-slate-800 text-white">
-          All rights reserved 2023.
-        </div>
       </div>
     </QueryClientProvider>
   );
--- src/routes/Index.tsx
+++ src/routes/Index.tsx
@@ -1,5 +1,4 @@
 import { useState } from "react";
-import MyIcon from "../assets/MyIcon";
 
 function Index() {
   const PROJECTS = [
@@ -28,49 +27,21 @@ function Index() {
   const [projects] = useState(PROJECTS);
 
   return (
-    <div className="px-24 max-w-7xl h-full mx-auto lg:px-0">
-      <div className="flex flex-col">
-        <div className="mt-36 px-6 lg:my-8 mx-4 text-4xl leading-normal lg:text-5xl lg:leading-relaxed  text-white ">
-          <h1>
-            I’m <span>Chris</span>, an interdisciplinary
-            <br />
-            <span>software engineer </span>focused
-            <br /> on crafting meaningful <span>experiences</span>.
-          </h1>
+    <div className="px-4 bg-indigo-900">
+      <div className="container flex gap-2 py-4">
+        <div className="mx-auto max-w-2xl w-full">
+          <h2 className="text-7xl text-white uppercase">Chris Movassagh</h2>
         </div>
-        <div className="flex flex-col gap-12 my-24">
-          <h2 className="mx-4 px-6 mt-6 text-2xl lg:text-3xl text-white font-semibold">
-            Some of my works
-          </h2>
-          {projects.map((project, index) => {
-            return (
-              <a
-                key={index}
-                href={project.deplyed_url}
-                className="peer py-14 px-8 mx-8 border text-white rounded-lg transition duration-150 hover:py-20 hover:px-12 hover:mx-4 hover:text-black hover:bg-white hover:ease-in-out hover:duration-700"
-              >
-                <h4 className="font-semibold text-xl lg:text-2xl uppercase">
-                  {project.project_name}
-                </h4>
-                <p className="my-2">{project.project_description}</p>
-                <p className="uppercase">{project.project_technologies}</p>
-              </a>
-            );
-          })}
-        </div>
-        <div className="mx-4 my-12 text-white">
-          <h4 className="text-2xl ">Drop me a line</h4>
-          <div className="mt-12 grid grid-cols-3 grid-rows-2 gap-4 uppercase">
-            <MyIcon />
-            <h4>Github</h4>
-            <a
-              href="https://www.linkedin.com/in/chris-movassagh/"
-              className="flex group"
-            >
-              <MyIcon />
-              <h4>LinkedIn</h4>
-            </a>
-          </div>
+        <div className="mx-auto max-w-2xl w-full">
+          <h4 className="text-2xl text-neutral-500">
+            Back in 2012, I decided to try my hand at creating custom Tumblr
+            themes and tumbled head first into the rabbit hole of coding and web
+            development. Fast-forward to today, and I’ve had the privilege of
+            building software for an advertising agency, a start-up, a huge
+            corporation, and a digital product studio. My main focus these days
+            is building accessible user interfaces for our customers at Klaviyo.
+            I most enjoy building soft
+          </h4>
         </div>
       </div>
     </div>
